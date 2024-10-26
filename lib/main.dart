495ee53117b0b31de4bcdb9b6f3d37ecb6537616
dprouet <damienprouet76@gmail.com>
2024-10-26 13:49:20 +0200

    Variables and Printing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 const age = 26;
+const twiceTheAge = age * 2;
+
+String getFullName(String firstName, String lastName) {
+  return firstName + lastName;
+}
 
 void main() {
-  final name = 'Foo';
 
   runApp(const MyApp());
 }
@@ -14,6 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(getFullName('Foo', 'Bar'));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
