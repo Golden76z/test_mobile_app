d255536c3ef412b08fb6de3dbe136de44f87be60
dprouet <damienprouet76@gmail.com>
2024-11-12 14:30:30 +0100

    Switch case in dart

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,7 +1,25 @@
+
+void printProperties(String firstName, String lastName) {
+  print(Info.lastName);
+}
+
 enum Info {
   firstName, lastName, age
 }
 
-void printProperties(String firstName, String lastName) {
-  print(Info.lastName);
+enum AnimalType {
+  cat,
+  dog,
+  bunny,
+}
+
+void testAnimal(AnimalType animalType) {
+  switch (animalType) {
+    case AnimalType.cat:
+      print("cat");
+    case AnimalType.dog:
+      print("dog");
+    case AnimalType.bunny:
+      print("bunny");    
+  }
 }
